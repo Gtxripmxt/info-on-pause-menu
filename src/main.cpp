8c3c5b247521c31ff70c7238c5b45af9c72b10d2
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-13 09:48:27 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/PauseLayer.hpp>
+class $modify(MyPauseLayer, PauseLayer) {
+	void customSetup() {
+		// Call original setup
+		PauseLayer::customSetup();
+
+		log::info("customSetup override called");
+
+		auto infoButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"),
+			this,
+			menu_selector(MyPauseLayer::onInfoButton)
+		);
+
+		auto menu = this->getChildByID("center-button-menu");
+		if (!menu) {
+			log::warn("center-button-menu not found!");
+			return;
+		}
+
+		infoButton->setID("info-button"_spr);
+		menu->addChild(infoButton);
+
+		menu->updateLayout();
+	}
+
+	void onInfoButton(CCObject*) {
+		FLAlertLayer::create("Ok", "OK", "OK")->show();
+	}
+};
